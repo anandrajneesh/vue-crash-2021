349dbab74ce549be8489163c69059cb9dbc61b98
Anand Rajneesh <anandrajneesh@outlook.com>
2021-08-29 12:48:13 +0530

    Initial commit with view tasks, toggle reminder and delete task

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+<div class="container">
+  <Header title="Task Tracker"/>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+</div>
+    
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+    tasks : []
+    }
+  },
+  methods:{
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => {
+        console.log(task.reminder)
+        return task.id !== id
+        })
+    },
+    toggleReminder(id){
+      console.log(id)
+      this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
+    }
+  },
+  created() {
+    this.tasks = [
+        {
+          id: 1,
+          text: "Finish Vue",
+          reminder: true,
+          day : "Sep 14 at 12:00am"
+        },
+        {
+          id: 2,
+          text: "Learn more technical analysis",
+          reminder: true,
+          day : "Oct 14 at 12:00am"
+        },
+        {
+          id: 3,
+          text: "Get Fit",
+          reminder: false,
+          day : "Nov 14 at 12:00am"
+        }
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>{{title}}</h1>
+    <Button text="Add Text" color="green"/>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+    name: 'Header',
+    props:{
+        title: String
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+ header{
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     margin-bottom: 20px;
+ }
+</style>
